ff15514b4ebb05c6a0c345b01f14ac8f9b059f8a
suhan272 <bunnyjangiti165@gmail.com>
2026-08-03 21:02:52 +0530

    Time: 0 ms (100%), Space: 43 MB (51.46%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // int res="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s.ChatAt(i)>=65||s.ChatAt(i)<=90){
+        //         res=res+
+        //     }
+           return s.toLowerCase();
+        }
+        
+    }
